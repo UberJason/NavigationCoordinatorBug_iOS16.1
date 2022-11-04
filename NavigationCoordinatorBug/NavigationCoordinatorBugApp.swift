83ddf921ce9781f4ea297ae62aa13266295a17e0
Jason Ji <uberjason@gmail.com>
2022-11-03 20:04:01 -0400

    Initial Commit

---
+++ NavigationCoordinatorBug/NavigationCoordinatorBugApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationCoordinatorBugApp.swift
+//  NavigationCoordinatorBug
+//
+//  Created by Jason Ji on 11/3/22.
+//
+
+import SwiftUI
+
+@main
+struct NavigationCoordinatorBugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
